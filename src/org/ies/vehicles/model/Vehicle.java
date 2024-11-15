97e21ec1a1f842c61283f23d51c4f46f0122f918
Angel <angel.salgado@educa.madrid.org>
2024-11-15 14:27:10 +0100

    Implementado vehiculo

--- src/org/ies/vehicles/model/Vehicle.java
+++ src/org/ies/vehicles/model/Vehicle.java
@@ -1,4 +1,72 @@
 package org.ies.vehicles.model;
 
+import java.util.Objects;
+
 public class Vehicle {
+    private VehicleType vehicleType ;
+    private String color;
+    private int maxSpeed;
+    private String plate;
+
+    public Vehicle(VehicleType vehicleType, String color, int maxSpeed, String plate) {
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.plate = plate;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return maxSpeed == vehicle.maxSpeed && vehicleType == vehicle.vehicleType && Objects.equals(color, vehicle.color) && Objects.equals(plate, vehicle.plate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vehicleType, color, maxSpeed, plate);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleType=" + vehicleType +
+                ", color='" + color + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", plate='" + plate + '\'' +
+                '}';
+    }
 }
